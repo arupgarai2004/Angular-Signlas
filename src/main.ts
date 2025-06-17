f0f276ea8d2ebcab2d860dd8713de987fe8e3f1f
arupgarai2004 <arupgarai2004@gmail.com>
2025-06-17 22:42:37 +0200

    code update

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+    <h1>Hello from {{ name }} {{ count() }}!</h1>
+    <input type="button" (click)="increseVal()" value="Increse"/>
   `,
 })
 export class App {
   name = 'Angular';
+
+  count= signal(0);
+
+  increseVal(){
+    this.count.update(value=> value+1);
+  }
 }
 
 bootstrapApplication(App);
