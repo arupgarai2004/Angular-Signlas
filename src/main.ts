71017bd6878f8a36e284c22f391e1418723b8ba2
arupgarai2004 <arupgarai2004@gmail.com>
2025-06-20 11:41:26 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello from {{ name }} {{ count() }}!</h1>
+    <h1>Hello from {{ name }} {{ count() }}! {{sumValue()}}</h1>
     <input type="button" (click)="increseVal()" value="Increse"/>
   `,
 })
@@ -12,10 +12,17 @@ export class App {
   name = 'Angular';
 
   count= signal(0);
+  numberOne=signal(3);
+  numberTwo=signal(5);
 
   increseVal(){
     this.count.update(value=> value+1);
+    this.numberOne.update(value=> value + 5);
   }
+
+   sumValue  = computed(() => this.numberOne() + this.numberTwo());
+  
+  
 }
 
 bootstrapApplication(App);
